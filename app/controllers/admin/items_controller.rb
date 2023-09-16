2bb9c8b1b66d4e32edc412180338e2c5ea01bc80
EC2 Default User <ec2-user@ip-172-31-46-53.ap-northeast-1.compute.internal>
2023-09-16 14:53:59 +0000

    投稿画面メソッドエラー

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,14 +1,19 @@
 class Admin::ItemsController < ApplicationController
   def new
+    @item = Item.new
   end
   
   def index
+    @items = Items.all
   end
   
   def show
   end
   
   def create
+    @item = Item.new(item_params)
+    item.save
+    redirect_to admin_items_path
   end
   
   def edit
@@ -16,4 +21,8 @@ class Admin::ItemsController < ApplicationController
   
   def update
   end
+  
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :image, :price)  end
 end
