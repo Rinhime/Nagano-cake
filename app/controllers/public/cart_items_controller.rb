f3eb50ce1b2617983927c2e042f94bfd594552f5
EC2 Default User <ec2-user@ip-172-31-46-53.ap-northeast-1.compute.internal>
2023-09-22 07:52:48 +0000

    カート追加作成中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,8 +1,14 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = CartItem.all
+    
   end
   
   def create
+    @cart_item = CartItem.find(params[:item_id])
+    @item = @cart_item.items.find(params[:item][:id])
+    @cart_item.create(cart_item_params)
+    redirect_to public_cart_items_path
   end
   
   def update
@@ -13,4 +19,11 @@ class Public::CartItemsController < ApplicationController
   
   def destroy_all
   end
+  
+  private
+ 
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+  end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -13,5 +13,8 @@ class Item < ApplicationRecord
     (self.price * 1.10).round
   end
   
+  def with_tax_price
+    (price * 1.1).floor
+  end
   
 end
