83b8391e4406e3791efd38f242c12cdab973e087
EC2 Default User <ec2-user@ip-172-31-46-53.ap-northeast-1.compute.internal>
2023-09-29 04:59:44 +0000

    注文確認画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,13 +1,18 @@
 class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
+    @postal_code = current_customer.postal_code
+    @address = current_customer.address
+    @last_name = current_customer.last_name
+    @first_name = current_customer.first_name
     @customer = current_customer
-    
   end
   
   def confirm
     @order = Order.new(order_params)
-    
+    @order.post_code = current_customer.post_code
+    @order.address = current_customer.address
+    @order.name = current_customer.last_name + current_customer.first_name
   end
   
   def index
@@ -28,7 +33,7 @@ class Public::OrdersController < ApplicationController
   private
   
     def order_params
-      params.require(:order).permit(:shipping_cost, :payment_method, :payment_price, :address, :postal_code, :name)
+      params.require(:order).permit(:payment_method, :shipping_cost, :payment_price, :postal_code, :address, :name)
     end
   
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,8 @@ class Order < ApplicationRecord
   has_many :order_item
   belongs_to :customer
   enum payment_method: { credit_card: 0, transfer: 1 }
+  enum select_address: { own_address: 0, registered_address: 1, new_address: 2 }
+  
   
   def address_display
   '〒' + current_customer.postal_code + ' ' + current_customer.address + ' ' + current_customer.last_name + ' ' + current_customer.first_name
